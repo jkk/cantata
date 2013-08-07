aa7251102f785e661318976569d0ea0fdb22d98e
Justin Kramer <jkkramer@gmail.com>
2013-08-07 14:35:40 -0400

    bump honeysql dep

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.flatland/ordered "1.5.1"]
-                 [honeysql "0.4.0"]
+                 [honeysql "0.4.1"]
                  [org.clojure/java.jdbc "0.3.0-alpha4"]
                  [c3p0/c3p0 "0.9.1.2"]])
