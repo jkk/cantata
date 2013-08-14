56934889e10d90e99edbcd6bc206c8293837fd4b
Justin Kramer <jkkramer@gmail.com>
2013-08-13 21:03:00 -0400

    remove superfluous code from reflect ns

--- src/cantata/reflect.clj
+++ src/cantata/reflect.clj
@@ -13,54 +13,45 @@
 
 (defn reflect-entities [ds]
   (when cu/*verbose*
-   (println "Reflecting for entities"))
- (with-open [conn ^Connection (jd/get-connection ds)]
-   (let [dbmeta ^DatabaseMetaData (.getMetaData conn)
-         tmetas (doall
-                  (jd/result-set-seq
-                    (.getTables dbmeta nil nil nil nil)))]
-     (doall
-       (for [tmeta tmetas
-             :when (= "TABLE" (:table_type tmeta))]
-         (let [tname (:table_name tmeta)
-               ename (identifier tname)]
-           ;; TODO: schema
-           {:name ename
-            :db-name tname}))))))
+    (println "Reflecting for entities"))
+  (with-open [conn ^Connection (jd/get-connection ds)]
+    (let [dbmeta ^DatabaseMetaData (.getMetaData conn)
+          tmetas (doall
+                   (jd/result-set-seq
+                     (.getTables dbmeta nil nil nil nil)))]
+      (doall
+        (for [tmeta tmetas
+              :when (= "TABLE" (:table_type tmeta))]
+          (let [tname (:table_name tmeta)
+                ename (identifier tname)]
+            ;; TODO: schema
+            {:name ename
+             :db-name tname}))))))
 
 (defn reflect-fields [ds table]
- (when cu/*verbose*
-   (println "Reflecting for" table "fields"))
- (with-open [conn ^Connection (jd/get-connection ds)]
-   (let [dbmeta ^DatabaseMetaData (.getMetaData conn)
-         cmetas (doall
-                  (jd/result-set-seq
-                    (.getColumns dbmeta nil nil table nil)))
-         cmetas (if (seq cmetas) ;H2 quirk
-                  cmetas
-                  (doall
-                    (jd/result-set-seq
-                      (.getColumns dbmeta nil nil (string/upper-case table) nil))))]
-     (doall
-       (for [cmeta cmetas]
-         (let [cname (:column_name cmeta)
-               field-name (identifier cname)]
-           ;; TODO: type
-           {:name field-name
-            :db-name cname}))))))
+  (when cu/*verbose*
+    (println "Reflecting for" table "fields"))
+  (with-open [conn ^Connection (jd/get-connection ds)]
+    (let [dbmeta ^DatabaseMetaData (.getMetaData conn)
+          cmetas (doall
+                   (jd/result-set-seq
+                     (.getColumns dbmeta nil nil table nil)))]
+      (doall
+        (for [cmeta cmetas]
+          (let [cname (:column_name cmeta)
+                field-name (identifier cname)]
+            ;; TODO: type
+            {:name field-name
+             :db-name cname}))))))
 
 (defn reflect-rels [ds table]
   (when cu/*verbose*
-   (println "Reflecting for" table "rels"))
+    (println "Reflecting for" table "rels"))
   (with-open [conn ^Connection (jd/get-connection ds)]
     (let [dbmeta ^DatabaseMetaData (.getMetaData conn)
           imetas (doall
                    (jd/result-set-seq
-                     (.getImportedKeys dbmeta nil nil table)))
-          imetas (if (seq imetas) ;H2 quirk
-                   imetas
-                   (jd/result-set-seq
-                     (.getImportedKeys dbmeta nil nil (string/upper-case table))))]
+                     (.getImportedKeys dbmeta nil nil table)))]
       (doall
         (for [imeta imetas]
           (let [key (identifier (:fkcolumn_name imeta))]
@@ -71,19 +62,15 @@
              :other-key (identifier (:pkcolumn_name imeta))}))))))
 
 (defn reflect-pk [ds table]
- (when cu/*verbose*
-   (println "Reflecting for" table "PK"))
- (with-open [conn ^Connection (jd/get-connection ds)]
-   (let [dbmeta ^DatabaseMetaData (.getMetaData conn)
-         imetas (doall
+  (when cu/*verbose*
+    (println "Reflecting for" table "PK"))
+  (with-open [conn ^Connection (jd/get-connection ds)]
+    (let [dbmeta ^DatabaseMetaData (.getMetaData conn)
+          imetas (doall
                    (jd/result-set-seq
                      (.getPrimaryKeys dbmeta nil nil table)))
-         imetas (if (seq imetas) ;H2 quirk
-                  imetas
-                  (jd/result-set-seq
-                    (.getPrimaryKeys dbmeta nil nil (string/upper-case table))))
-         pks (for [imeta imetas]
-               (identifier (:column_name imeta)))]
-     (if (= 1 (count pks))
-       (first pks)
-       (vec pks)))))
+          pks (for [imeta imetas]
+                (identifier (:column_name imeta)))]
+      (if (= 1 (count pks))
+        (first pks)
+        (vec pks)))))
