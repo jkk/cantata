dc102ccbb4714fad1ccf7e0c15ac33b3908dd1c3
Justin Kramer <jkkramer@gmail.com>
2013-08-23 15:11:49 -0400

    cut 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cantata "0.1.0-SNAPSHOT"
+(defproject cantata "0.1.0"
   :description "Database abstraction library"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
