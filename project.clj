94006b448ce8c759294d1c6c53e287b81828ab12
Justin Kramer <jkkramer@gmail.com>
2013-10-24 17:25:37 -0400

    project url

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject cantata "0.1.8-SNAPSHOT"
   :description "SQL and database abstraction"
+  :url "https://github.com/jkk/cantata"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
